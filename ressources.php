d372426de6ffd2faa0ccaaec747005a387712cc8
DrPins <phuveteau@gmail.com>
2018-04-25 16:25:38 +0200

    modification ressources

--- ressources.php
+++ ressources.php
@@ -12,12 +12,18 @@
 <body>
 
   <div class="admin_home">
-    <h1>Ressources</h1>
-    <h3>Code (GitHub)</h3>
+    <h1>Ressources PPE2 - Site marchand</h1>
+    <a href="https://github.com/DrPins/PPE2_Mygavolt"><h3>Code (GitHub)</h3></a>
+    <a href="http://ppe2.perrine-huveteau.tk/index.php"><h3>Site web</h3></a>
+    <h5>Login test@mygavolt.fr   / Mot de passe : test</h5>
+    <a href="http://ppe2.perrine-huveteau.tk/admin/index.php"><h3>Site web - Administration</h3></a>
+    <h5>Login Admin   / Mot de passe : Admin</h5>
+    <a href="https://goo.gl/eajf2Y"><h3>Ressources additionelles</h3></a>
 
-    <h3>Site web</h3>
-
-    <h3>Ressources additionelles</h3>
+    <h1>Ressources PPE4 - Application Android</h1>
+    <a href="https://github.com/DrPins/PPE4_Mygavolt "><h3>Code Application (GitHub)</h3></a>
+    <a href="https://github.com/DrPins/API-Mygavolt"><h3>Code API (GitHub)</h3></a>
+    <a href="https://goo.gl/caELBR"><h3>Ressources additionelles</h3></a>
 
 
   </div>

53224f65d61cecedb501f4910a050434b5c2c6ef
DrPins <phuveteau@gmail.com>
2018-05-24 09:22:55 +0200

    ajout géoloc + PF

--- admin/geoloc.php
+++ admin/geoloc.php
@@ -1,7 +1,8 @@
 
 
-<?php
 
+<?php
+//AIzaSyBOYpM5-5dcQSsoisKN8ouo9dD4giC66Vk
 
 require_once('header.php');
 
@@ -18,13 +19,22 @@ $timestamp   = time();
             die('<h1>Impossible de se connecter</h1>');
         }?>
 
-<div class="admin_home">Bienvenue <?php echo $_SESSION['username'] ?><br>
-
+<div class="admin_home">
+  <h3>Geolocalisation des employés</h3>
 
   <?php
         $select = $db->prepare("SELECT * from coordinates where date_position in (select MAX(date_position) from coordinates group by id_employee)");
 
-        $select->execute();?>
+        $select->execute();
+
+
+        ?>
+
+        <script >
+          var locations = [];
+        </script>
+
+
 
         <table class="table">
           <tr>
@@ -35,13 +45,25 @@ $timestamp   = time();
           </tr>
           <?php
         while($s=$select->fetch(PDO::FETCH_OBJ)){
+
           $c = $s->lat;
           $pos1 = strrpos($c, "(");
           $pos2 = strrpos($c, ",");
           $pos3 = strrpos($c, ")");
           $lat = substr($c, $pos1+1, $pos2-$pos1-1);
           $lng = substr($c, $pos2+1, $pos3-$pos2-1);
-            ?>
+          ?>
+            <script>
+
+
+              locations.push(
+                {lat: <?php echo $lat;?>, lng: <?php echo $lng;?>}
+              );
+
+             // alert("lat:"+<?php echo $lat;?>+", lng:" +<?php echo $lng;?>);
+
+            </script>
+
             <tr>
               <td><?php echo $s->id_employee;?></td>
               <td><?php echo $s->date_position;?></td>
@@ -52,6 +74,64 @@ $timestamp   = time();
                 ?>
 
           </table>
+
+
+
+
+
+<div id="map"></div>
+    <script>
+
+      function initMap() {
+
+        //alert(locations);
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 9,
+          center: {lat: 50.633333, lng: 3.066667}
+        });
+
+        // Create an array of alphabetical characters used to label the markers.
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        // Add some markers to the map.
+        // Note: The code uses the JavaScript Array.prototype.map() method to
+        // create an array of markers based on a given "locations" array.
+        // The map() method here has nothing to do with the Google Maps API.
+        var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+          });
+        });
+
+
+
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      }
+
+    </script>
+    <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js">
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBOYpM5-5dcQSsoisKN8ouo9dD4giC66Vk&callback=initMap">
+    </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </div>
 
 
